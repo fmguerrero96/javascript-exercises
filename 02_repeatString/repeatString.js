0c3f0ae5de85d06ded812977dd2f6796343940ac
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-10 12:41:12 -0500

    Complete exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    word = string
+    result = ''
+    i = 0
+    if (num < 0) { result = 'ERROR'};
+        while (i < num) {
+          result += word;
+          i = i + 1;
+        } return result;
 };
 
 // Do not edit below this line
