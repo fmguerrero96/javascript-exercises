36cd438c6991bb404eece880907549e5ec620975
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-12 18:01:37 -0500

    complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function sumAll(a, b){
+    let small = Math.min(a,b);
+    let max = Math.max(a,b);
+    let sum = 0;
+    const message = "ERROR"
+    
+    if ((a<0) || (b<0)){return message};
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return message;
+    
+    while (small <= max){
+      sum += small;
+      small += 1 
+    } return sum
+  }
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
