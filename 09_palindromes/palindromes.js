ce5dfd829b53a0c80adef5060bb50f019dd1a956
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-25 12:38:06 -0500

    complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const noSymbol = phrase.replace(/[^a-z0-9]/gi, '');
+    const lower = noSymbol.toLowerCase();
+    let splitting = lower.split('');
+    let rev = splitting.reverse();
+    let backToStr = rev.join('');
+    return (backToStr == lower)
 };
 
 // Do not edit below this line
