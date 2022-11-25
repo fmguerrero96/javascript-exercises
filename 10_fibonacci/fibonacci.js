1a61063f2d455b962bddf8d53072159651d51b2e
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-25 13:12:53 -0500

    complete fiboancci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const numbers = [1, 1];
+    previous = 1;
+    current = 1;
+    result = 0;
+    count = 0;
+    while (count < 30){
+        result = previous + current;
+        numbers.push(result);
+        previous = current;
+        current = result;
+        count += 1
+    }
+    if (num < 0){
+        return ('OOPS')
+    }else if (num === 0){
+        return 0
+    }else {
+        return numbers[num - 1]
+    }
 };
 
 // Do not edit below this line
