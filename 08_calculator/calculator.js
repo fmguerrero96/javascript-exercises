0259310f5d4bfdacf90f0c5c2d5f83b50d1c19bc
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-25 12:01:04 -0500

    complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a+b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	const big = Math.max(a,b);
+  const small = Math.min(a,b);
+  return (big - small)
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	result = 0
+  for (let i = 0; i<nums.length; i++){
+    result += nums[i];
+  } return result
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+  ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1)
+  return 1;
+  for (let i = num - 1; i >= 1; i--) {
+  num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
