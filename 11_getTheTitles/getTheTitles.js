c29fac2a22729f3848a9ba482502bf561d4269f4
Misael Guerrero <mguerrerowest@gmail.com>
2022-11-26 21:13:28 -0500

    complete getTheOldest and getTheTitles exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let getBooks = array.map(book => book.title);
+    return getBooks
 };
 
 // Do not edit below this line
